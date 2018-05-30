2c2aa2aea582f89819e8fa6d0ce64ffa59835e00
Peter Henry <me@peterhenry.net>
2018-05-30 16:30:44 -0700

    add i3 mode_display to run arandr scripts

--- i3/.config/i3/buildconfig.sh
+++ i3/.config/i3/buildconfig.sh
@@ -3,8 +3,9 @@
 COLORS="/home/phenry/.config/i3/colors/gruvbox"
 BAR="/home/phenry/.config/i3/bars/gruvbox"
 VOLUME="/home/phenry/.config/i3/volume/pulseaudio-ctl"
+DISPLAYS="/home/phenry/.config/i3/displays"
 BASE="/home/phenry/.config/i3/base"
 
 OUTPUT="/home/phenry/.config/i3/config"
 
-cat $COLORS $BAR $VOLUME $BASE > $OUTPUT
+cat $COLORS $BAR $VOLUME $DISPLAYS $BASE > $OUTPUT
