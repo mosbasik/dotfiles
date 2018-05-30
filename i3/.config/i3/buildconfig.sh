4ec2ee10504bf38e5602cf44429f99c593f68f8e
Peter Henry <me@peterhenry.net>
2018-05-30 15:37:26 -0700

    start building i3 config with a script

---
+++ i3/.config/i3/buildconfig.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+COLORS="/home/phenry/.config/i3/colors/gruvbox"
+   BAR="/home/phenry/.config/i3/bars/gruvbox"
+VOLUME="/home/phenry/.config/i3/volume/pulseaudio-ctl"
+  BASE="/home/phenry/.config/i3/base"
+
+OUTPUT="/home/phenry/.config/i3/config"
+
+cat $COLORS $BAR $VOLUME $BASE > $OUTPUT
